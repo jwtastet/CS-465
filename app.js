a3ef898255f19232018aeb5927c4ce2bc980855c
Joseph Tastet <josephwtastet@gmail.com>
2023-05-18 15:17:02 -0400

    What a mess... Cleaned up routes, controllers, and required hbs in the app.js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel'); 
 
-var app = express();
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views/partials'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials (https://www.npmjs.com/packjage/hbs)
-hbs.registerPartials(path.join(_dirname, 'app_server', 'views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
