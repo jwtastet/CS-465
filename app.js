a382bd8bf2d3a318ff908e9d2fba0ba1ef1ce875
Joseph Tastet <josephwtastet@gmail.com>
2023-05-18 11:36:44 -0400

    fixed app.js post module 2 rubric

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views/partials'));
+
+// register handlebars partials (https://www.npmjs.com/packjage/hbs)
+hbs.registerPartials(path.join(_dirname, 'app_server', 'views/partials'));
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
